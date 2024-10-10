dc40d594673a93140c3de39d712c9343a948a4c8
Boris Gesbert <b.s.i.gesbert@tudelft.nl>
2024-10-10 15:26:25 +0200

    Gave a meaningful name to the subtraction function

--- src/operations.py
+++ src/operations.py
@@ -5,8 +5,7 @@ Arithmetic operations
 def summation(a, b):
     return a + b
 
-def sub(a, b):
-    #TODO: would be good to give this function a better name
+def subtraction(a, b):
     return a - b
 
 def division(a, b):
